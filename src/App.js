8e6e8fab588af4bdae257abf5a534288d869248d
skaramje <skaramje@gmail.com>
2021-10-25 19:54:52 -0600

    add a formik form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useFormik} from 'formik'
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+    onSubmit: values =>{
+      console.log('form:', values);
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>Name</div>
+      <input name="name" type="text" onChange={formik.handleChange} value={formik.values.name}/>
+      <div>Email</div>
+      <input name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+      <div>Password</div>
+      <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>
+      <button type="submit">Submit</button>
+    </form>
     </div>
   );
 }
